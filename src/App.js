1d6b1fa26620fc3d26a05fd2ad16851beae91337
Elena <smart.puffin.dev@gmail.com>
2022-04-26 21:20:04 +0200

    reproducing the bug in Android Chrome version 100.0.4896.127

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+class Popup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isOpen: false };
+  }
+  openOrClose = () => {
+    var open = !this.state.isOpen;
+    this.setState({ isOpen: open });
+  }
+  close = () => {
+    this.setState({ isOpen: false });
+  }
+  render() {
+    if (!this.state.isOpen)
+      return (
+        <button onClick={(e) => { this.openOrClose() }}>
+          Step1: Open popup
+        </button>);
+
+    return (
+      <div style={{ position: 'absolute', background: 'yellow', width: '300px', height: '300px' }}>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+function InputWithValue() {
+  var [step, setStep] = useState(1);
+  return (
+    <div>
+      Step2: click 'next'; step3: see the input field (or empty screen on Android Chrome)
+      {step === 1 && <button onClick={() => { setStep(2) }}>Next step</button>}
+      {step === 2 && <input value="123" onChange={()=>{}} />}
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Welcome to this convoluted example!</p>
+
+      {/*
+        0 or 2+ text fields: all works
+        1 text field: doesn't work
+        */}
+      <input value="input111" onChange={()=>{}} />
+
+      {/* <input value="input222" onChange={()=>{}} /> */}
+
+      <Popup>
+        <InputWithValue />
+      </Popup>
     </div>
   );
 }
